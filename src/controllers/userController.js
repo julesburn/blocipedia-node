0d584b213652f22c8be6a555f34b357579ac170a
julesburn <cmbenson88@gmail.com>
2019-08-13 13:22:40 -0500

    Adds logs to user controller for troubleshooting

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -128,16 +128,19 @@ downgrade(req, res, next) {
 },
 
 showCollaborators(req, res, next) {
+  console.log(req);
+  console.log("showing collaborators");
   userQueries.getUserCollaborators(req.user.id, (err, result) => {
     const collaborator = result.collaborator;
     const user = result.user;
-
-      if (err) {
-        console.log("There was an error: ", err)
+    console.log(result);
+      if (err || result == null) {
+        console.log(err);
           res.redirect(404, "/");
       } else {
+        console.log("rendering view");
           res.render("users/collaborators", { user, collaborator });
       }
   });
 }
-};
+}
